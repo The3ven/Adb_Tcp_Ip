19ac91a9f784b3032d5c39a2c94d97e87be35328
THE EVEN <96623398+The3ven@users.noreply.github.com>
2023-01-27 03:14:04 +0530

    Update adb_tcp_ip.ps1

--- adb_tcp_ip.ps1
+++ adb_tcp_ip.ps1
@@ -4,7 +4,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/The3ven/Adb_Tcp_Ip/main/adb_tcp_ip.bat'
+$DownloadURL = 'https://raw.githubusercontent.com/The3ven/Adb_Tcp_Ip/main/adb_tcp_ip.cmd'
 
 $FilePath = "$env:TEMP\adb_tcp_ip.cmd"
 $ScriptArgs = "$args "
